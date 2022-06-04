82dc75ce0821ac55cb30ebbf0ca9d4c3437f3a97
KhairienNissa <cacakhairien@gmail.com>
2022-06-04 12:57:24 +0700

    update

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const express = require('express')
+//import
+require('dotenv').config()
 
+const express = require('express')
 const cors = require('cors')
 
+// import socket
 const http = require('http')
 const {Server} = require('socket.io')
 
-require('dotenv').config()
-
-//Get routes to the variabel here
-const router =  require("./src/routes")
+const router = require('./src/routes')
 
 const app = express()
 
+// socket
 const server = http.createServer(app)
 const io = new Server(server, {
  cors: {
-          origin: process.env.CLIENT_URL ||
-          "dumbsound-khairien.netlify.app" ||
-          "http://localhost:3000"  // define client origin if both client and server have different origin
+  origin: process.env.CLIENT_URL ||
+          "https://dumbsound-khairien.netlify.app" ||
+          "http://localhost:3000" //port client
  }
 })
 
 require('./src/socket')(io);
 
+// PORTg
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
-//Create endpoint grouping and router here
-app.use("/api/v1/", router)
-app.use('/uploads', express.static('uploads'))
 
+app.use('/api/v1/', router) //url
+app.use('/uploads', express.static('uploads'));
+
+// listen port
 server.listen(port, () => console.log(`Listening on port ${port}!`))
-
