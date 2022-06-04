a14baca791d97fde498c5b63fd9905a96400e7e5
KhairienNissa <cacakhairien@gmail.com>
2022-06-04 13:04:55 +0700

    update

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 exports.uploadFile = function () {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "uploads");
+      cb(null);
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + "-" + file.originalname.replace(/\s/g, ""));
